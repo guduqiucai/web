e717ebf08c3af1f7e06795ff2b742ab94a483231
yuanbeibei <yuanbb@gresgying.com>
2023-01-10 09:30:07 +0800

    fix11

--- Engineering/my-module/lib/__tests__/myModule.test.js
+++ Engineering/my-module/lib/__tests__/myModule.test.js
@@ -1,8 +1,8 @@
-const assert = require('assert');
-const myModule = require('../index.js');
+const assert = require("assert");
+const myModule = require("../index.js");
 
-describe('myModule', () => {
-  it('has a test', () => {
-    assert(false, 'myModule should have a test');
+describe("myModule", () => {
+  it("has a test", () => {
+    assert(false, "myModule should have a test");
   });
 });
